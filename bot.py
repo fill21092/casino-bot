6d61920c6eb4283f451485cd3102e60c0d8e9663
fill21092 <danila.filatov.21@mail.ru>
2025-04-21 23:42:18 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot("7701797337:AAGCwk15d17a2v8RslBtvOLIJDOSm2eEdO0")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    markup = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton("🎲 Крутить рулетку", url="https://fill21092.github.io/casino-bot/")
+    button2 = types.InlineKeyboardButton("🎁 Получить бонус", url="https://your-bonus-link.com")
+    markup.add(button1)
+    markup.add(button2)
+
+    with open("static/banner.png", "rb") as photo:
+        bot.send_photo(message.chat.id, photo, caption="🎰 Добро пожаловать в Казино!
+💥 500% к первому депозиту + 75 фриспинов!", reply_markup=markup)
+
+bot.polling()
